d7492a8f8848fdfa4f04c09e09b849000f5a9334
Kaua10710 <kaduzerah06@gmail.com>
2024-12-20 17:32:10 -0300

    Iniciando classe pagamentos

---
+++ src/main/java/academia/Pagamentos.java
@@ -0,0 +1,10 @@
+
+package academia;
+
+c class Pagamentos {
+     private int idPagamento; 
+    private String dataPagamento; 
+    private double valorPagamento; 
+    private String formaPagamento; 
+    private int idAluno;
+}
