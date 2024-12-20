ab997f35291d4bde8bca4aa526c45d1aa77eaa2b
Kaua10710 <kaduzerah06@gmail.com>
2024-12-20 17:36:44 -0300

    Update Pagamentos.java

--- src/main/java/academia/Pagamentos.java
+++ src/main/java/academia/Pagamentos.java
@@ -2,9 +2,13 @@
 package academia;
 
 c class Pagamentos {
-     private int idPagamento; 
-    private String dataPagamento; 
-    private double valorPagamento; 
-    private String formaPagamento; 
-    private int idAluno;
+     private int idPagamento; // Identificador único do pagamento
+    private String dataPagamento;  // Data em que o pagamento foi realizado
+    private double valorPagamento; // Valor do pagamento 
+    private String formaPagamento;  // Forma de pagamento (ex: cartão, dinheiro, pix)
+    private int idAluno; // identificador do aluno relacionado ao pagamento 
+    
+    // Construtor para inicializat os atributos
+    public Pagamentos(int idPagamento, String dataPagamento, double valorPagamento, String formaPagamento, int idAluno) {
+        
 }
