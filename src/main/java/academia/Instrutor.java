11b1333d97020095102e957d9403f916142819c7
Dheniel Rodrigues Luis <marcos.pessoa@estudante.ifgoiano.edu.br>
2024-12-20 21:44:48 -0300

    criação da classe instrutor

---
+++ src/main/java/academia/Instrutor.java
@@ -0,0 +1,19 @@
+package academia;
+
+/**
+ *
+ * @author Marcos Felipe
+ * @since 20/12/2024 9:39 PM
+ */
+public class Instrutor {
+    private int id;
+    private String nome;
+    private String especificidade;
+    
+    Instrutor(){
+    }
+    
+    public String getNome(){
+        return this.nome;
+    } 
+}
