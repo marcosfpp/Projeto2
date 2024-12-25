5221a716dc6d3267c54678aca3839871638c2bb7
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2024-12-25 10:13:40 -0300

    Criação de novos atributos e métodos

--- src/main/java/academia/Aluno.java
+++ src/main/java/academia/Aluno.java
@@ -1,17 +1,46 @@
 package academia;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Dheniel
  * @since 19/12/2024 20:00 PM
  */
 public class Aluno {
-    private int aluno;
-    private String nome;
-    private int idade;
-    private String email;
-    private boolean statusMatricula;
+
+    private int idAluno;
+    private String nomeAluno;
+    private int idadeAluno;
+    private String emailAluno;
+    private boolean statusMatriculaAluno; 
     
-    public Aluno(){
+    /**
+     * 
+     * @return Método construtor para forçar a entrada dos atributos na classe
+     * @param idAluno recebe o id do aluno para identificação precisa
+     * @param nomeAluno nome do aluno
+     * @param idadeAluno recebe a idade do aluno
+     * @param emailAluno recebe o email do aluno
+     * @param statusMatriculaAluno recebe a situação da matricula do aluno
+     */
+    Aluno(int idAluno, String nomeAluno, int idadeAluno, String emailAluno, boolean statusMatriculaAluno) {
+        this.idAluno = idAluno;
+        this.nomeAluno = nomeAluno;
+        this.idadeAluno = idadeAluno;
+        this.emailAluno = emailAluno;
+        this.statusMatriculaAluno = false;
+    }
+    /**
+     * 
+     * @return Método que verifica se o cliente efetuou o pagamento para definir a situação de mátricula como verdadeira
+     * @param situacaoPagamento situação se o aluno efetuou o ou não o pagamento
+     * @param statusMatriculaAluno define o status da matrícula
+     */
+    public void setRenovarMatricula(boolean situacaoPagamento){
+        if(situacaoPagamento){
+            this.statusMatriculaAluno = true;
+        } 
     }
 }
--- src/main/java/academia/Pagamentos.java
+++ src/main/java/academia/Pagamentos.java
@@ -2,11 +2,12 @@
 package academia;
 
 public class Pagamentos {
-     private int idPagamento; // Identificador único do pagamento
+    private int idPagamento; // Identificador único do pagamento
     private String dataPagamento;  // Data em que o pagamento foi realizado
     private double valorPagamento; // Valor do pagamento 
     private String formaPagamento;  // Forma de pagamento (ex: cartão, dinheiro, pix)
     private int idAluno; // identificador do aluno relacionado ao pagamento 
+    private boolean situacaoPagamento;
     
     // Construtor para inicializat os atributos
     public Pagamentos(int idPagamento, String dataPagamento, double valorPagamento, String formaPagamento, int idAluno) {
@@ -15,6 +16,7 @@ public class Pagamentos {
         this.valorPagamento = valorPagamento;
         this.formaPagamento = formaPagamento;
         this.idAluno = idAluno;
+        this.situacaoPagamento = false;
     }
     
     // Getters e Setters
@@ -44,6 +46,12 @@ public class Pagamentos {
     }
     public void setIdAluno(int idAluno) {
         this.idAluno = idAluno;
+    }
+    public void setPagarMensalidade(boolean mensalidade){
+        this.situacaoPagamento = true;      
+    }
+    public boolean getSiuacaoPagamento(){
+        return this.situacaoPagamento;
     }
     
     //Método para exibir os detalhes do pagamento 
