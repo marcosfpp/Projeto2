7a37842e853504debcf76b98789a4bf636a02bdb
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2025-01-02 21:53:43 -0300

    Criação de método para inserir pagamentos no banco
    Mudança de lógica, acarretando na exclusão de atributos e métodos

--- src/main/java/academia/Pagamentos.java
+++ src/main/java/academia/Pagamentos.java
@@ -1,20 +1,22 @@
-
 package academia;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Pagamentos {
     private int idPagamento; // Identificador único do pagamento
     private String dataPagamento;  // Data em que o pagamento foi realizado
-    private double valorPagamento; // Valor do pagamento 
     private String formaPagamento;  // Forma de pagamento (ex: cartão, dinheiro, pix)
-    private boolean situacaoPagamento;
+    private int idAluno;
     
     // Construtor para inicializat os atributos
-    public Pagamentos(int idPagamento, String dataPagamento, double valorPagamento, String formaPagamento, int idAluno) {
-        this.idPagamento = idPagamento;
+    public Pagamentos(String dataPagamento, String formaPagamento, int idAluno) {
         this.dataPagamento = dataPagamento;
-        this.valorPagamento = valorPagamento;
         this.formaPagamento = formaPagamento;
-        this.situacaoPagamento = false;
+        this.idAluno = idAluno;
+    }
+    Pagamentos(){
     }
     
     // Getters e Setters
@@ -30,20 +32,32 @@ public class Pagamentos {
     public double getValorPagamento(double valorPagamento) {
         return valorPagamento;
     }
-    public void setValorPagamento(double valorPagamento){
-        this.valorPagamento = valorPagamento;
-    }
     public String getFormaPagamento() {
         return formaPagamento;
     }
     public void setFormaPagamento (String FormaPagamento) {
         this.formaPagamento = formaPagamento;
     }
-    public void setPagarMensalidade(boolean mensalidade){
-        this.situacaoPagamento = true;      
-    }
-    public boolean getSiuacaoPagamento(){
-        return this.situacaoPagamento;
+   
+    public void inserirBanco() {
+        Connection conexao = new Conexao().getConexao();
+
+        String sql = "INSERT INTO pagamentos (id, dataPag, formaPag, id_aluno) VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setInt(1, this.idPagamento);
+            comando.setString(2, this.dataPagamento);
+            comando.setString(3, this.formaPagamento);
+            comando.setInt(4, this.idAluno);
+
+            comando.execute();
+            comando.close();
+
+            conexao.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar aluno: " + e.getMessage());
+        }
     }
     
     //Método para exibir os detalhes do pagamento 
@@ -52,7 +66,6 @@ public class Pagamentos {
         return "Pagamentos{" +
                 "idPagamento=" + idPagamento +
                 ", dataPagamento='" + dataPagamento + '\'' +
-                ", valorPagamento=" + valorPagamento +
                 ", formaPagamento='" + formaPagamento + '\'' +
                 '}';
     }
