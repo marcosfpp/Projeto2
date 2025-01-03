43e0fc99834388f9e03313ef850e610f619c47b2
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2025-01-02 23:03:29 -0300

    Criação de case de pagamentos

--- src/main/java/academia/Academia.java
+++ src/main/java/academia/Academia.java
@@ -6,49 +6,49 @@ import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class Academia {
-    
-    public static boolean verificarId(int idProcurado){
+
+    public static boolean verificarId(int idProcurado) {
         Connection conexao = new Conexao().getConexao();
         String sql = "SELECT COUNT(*) FROM aluno WHERE id = ?";
-        
-        boolean existe = false; 
-        
-        try{
+
+        boolean existe = false;
+
+        try {
             PreparedStatement comando = conexao.prepareStatement(sql);
             comando.setInt(1, idProcurado);
-            
-            try(ResultSet resultado = comando.executeQuery()){
-                if(resultado.next()){
+
+            try (ResultSet resultado = comando.executeQuery()) {
+                if (resultado.next()) {
                     int quantidade = resultado.getInt(1);
                     existe = quantidade > 0;
                 }
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Erro ao verificar id!" + e.getMessage());
         }
         return existe;
     }
-    
-    public static void validarMatricula(String matricula, int idAluno){
+
+    public static void validarMatricula(String matricula, int idAluno) {
         Connection conexao = new Conexao().getConexao();
         String sql = "UPDATE aluno SET matricula = ? WHERE id = ?";
-        
-        try{
+
+        try {
             PreparedStatement comando = conexao.prepareStatement(sql);
-            
+
             comando.setString(1, matricula);
             comando.setInt(2, idAluno);
-            
+
             comando.executeUpdate();
-            
+
             comando.close();
             conexao.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Erro ao validar matricula!" + e.getMessage());
         }
     }
-    
+
     public static void main(String[] args) {
         int opcMenu = 0;
         int posAlunos = 0;
@@ -72,35 +72,67 @@ public class Academia {
 
             switch (opcMenu) {
                 case 1:
-                    try{
+                    try {
                         System.out.println("\nCADASTRO DE ALUNOS\n");
-                        
-                        for(int i = 0; i < alunos.length; i++){
-                            if(alunos[i] != null){
+
+                        for (int i = 0; i < alunos.length; i++) {
+                            if (alunos[i] != null) {
                                 posAlunos++;
                             }
                         }
-                        
+
                         System.out.print("Digite o nome do aluno: ");
                         String nome = scan.nextLine();
-                        
+
                         System.out.print("\nDigite a idade do aluno: ");
                         int idade = scan.nextInt();
                         scan.nextLine();
-                        
+
                         System.out.print("\nDigite o email do aluno: ");
                         String email = scan.nextLine();
-                        
+
                         Aluno infoAlunos = new Aluno(nome, idade, email);
                         alunos[posAlunos] = infoAlunos;
                         alunos[posAlunos].inserirBanco();
                         scan.nextLine();
-                        
-                    }catch(Exception e){
+
+                    } catch (Exception e) {
                         System.out.println("\nErro ao tentar cadastrar aluno!" + e.getMessage());
                     }
                     break;
                 case 2:
+                    try {
+                        System.out.println("\nPAGAMENTO DE MENSALIDADE\n");
+                        System.out.println("VALOR = R$50,00");
+
+                        System.out.println("Digite o id do aluno");
+                        int idBusca = scan.nextInt();
+
+                        boolean existe = verificarId(idBusca);
+
+                        if (existe) {
+                            for (int i = 0; i < pagamentos.length; i++) {
+                                if (pagamentos[i] != null) {
+                                    posPag++;
+                                }
+                            }
+
+                            System.out.println("Digite a data atual: ");
+                            String dataAtual = scan.nextLine();
+
+                            System.out.println("Digite a forma de pagamento: ");
+                            String formaPagamento = scan.nextLine();
+
+                            Pagamentos infoPagamentos = new Pagamentos(dataAtual, formaPagamento, idBusca);
+                            pagamentos[posPag] = infoPagamentos;
+                            pagamentos[posPag].inserirBanco();
+                            validarMatricula("Ativa", idBusca);
+                        } else {
+                            System.out.println("O ID " + idBusca + " nao foi encontrado!");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Erro ao realizar pagamento" + e.getMessage());
+                    }
                     break;
                 case 3:
                     break;
@@ -117,7 +149,7 @@ public class Academia {
                     scan.nextLine();
                     break;
                 case 7:
-                    break;  
+                    break;
                 case 8:
                     break;
                 case 9:
