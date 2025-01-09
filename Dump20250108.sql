8214cd5574d8e6c0f2d7dcfb879a678f78a52c67
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2025-01-08 23:01:49 -0300

    Banco de dados

--- Dump20250108.sql
+++ Dump20250108.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `academia` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+USE `academia`;
 -- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: academia
@@ -16,6 +18,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `idade` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `matricula` varchar(255) NOT NULL,
+  `tipo_aula` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` VALUES (3,'dhenield',19,'dheniel@hotmail.com','Ativa','Crossfit'),(4,'pepe',22,'pepe@gmail.com','Ativa','Musculacao'),(5,'markin',19,'markin@gmail.com','Ativa','Musculacao'),(10,'sandro',15,'sandro@gmail.com','Ativa','Crossfit'),(11,'sandriel',15,'sandrieç@gmail.com','Ativa','Musculacao'),(12,'sandrinho',14,'sandrinho@gmail.com','Ativa','Musculacao');
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pagamentos`
 --
 
@@ -30,8 +60,18 @@ CREATE TABLE `pagamentos` (
   PRIMARY KEY (`id`),
   KEY `fk_aluno` (`id_aluno`),
   CONSTRAINT `fk_aluno` FOREIGN KEY (`id_aluno`) REFERENCES `aluno` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagamentos`
+--
+
+LOCK TABLES `pagamentos` WRITE;
+/*!40000 ALTER TABLE `pagamentos` DISABLE KEYS */;
+INSERT INTO `pagamentos` VALUES (5,'07/03/2006','pix',3),(6,'13/03/2002','cartao',4),(7,'22/10/2024','pix',3),(8,'01/01/2025','cartao',5),(12,'07/01/2025','pix',11),(13,'07/01/2025','pix',12);
+/*!40000 ALTER TABLE `pagamentos` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -42,4 +82,4 @@ CREATE TABLE `pagamentos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-03 21:58:13
+-- Dump completed on 2025-01-08 22:59:27
